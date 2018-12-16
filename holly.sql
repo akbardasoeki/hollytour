12324b7d211d1608f47054a46de5aa3018983be7
nabellapermata <nabellapermatas.if@gmail.com>
2018-12-16 13:27:18 +0700

    1634010048

--- holly.sql
+++ holly.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 14, 2018 at 12:50 PM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.1
+-- Generation Time: 16 Des 2018 pada 07.25
+-- Versi Server: 10.1.30-MariaDB
+-- PHP Version: 5.6.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,56 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `guide`
+--
+
+CREATE TABLE `guide` (
+  `id_guide` int(10) UNSIGNED NOT NULL,
+  `nama_guide` varchar(100) NOT NULL,
+  `jns_kelamin_guide` varchar(10) NOT NULL,
+  `no_telp_guide` varchar(20) NOT NULL,
+  `address_guide` varchar(100) NOT NULL,
+  `umur_guide` varchar(3) NOT NULL,
+  `date_booking` date NOT NULL,
+  `destinasi` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `order`
+--
+
+CREATE TABLE `order` (
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `paket` varchar(50) NOT NULL,
+  `kode_booking` varchar(50) NOT NULL,
+  `nama_guide` varchar(100) NOT NULL,
+  `no_rekening` varchar(50) NOT NULL,
+  `metode_bayar` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `paket`
+--
+
+CREATE TABLE `paket` (
+  `kode_paket` varchar(10) NOT NULL,
+  `nama_paket` varchar(100) NOT NULL,
+  `tujuan` varchar(100) NOT NULL,
+  `kuota` int(3) NOT NULL,
+  `destinasi_wisata` varchar(100) NOT NULL,
+  `durasi_paket` varchar(30) NOT NULL,
+  `harga_paket` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
@@ -41,7 +90,7 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
 INSERT INTO `user` (`name`, `username`, `no_telp`, `jns_kelamin`, `email`, `address`, `password`, `id`, `role`) VALUES
@@ -55,6 +104,24 @@ INSERT INTO `user` (`name`, `username`, `no_telp`, `jns_kelamin`, `email`, `addr
 --
 
 --
+-- Indexes for table `guide`
+--
+ALTER TABLE `guide`
+  ADD PRIMARY KEY (`id_guide`);
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`kode_booking`);
+
+--
+-- Indexes for table `paket`
+--
+ALTER TABLE `paket`
+  ADD PRIMARY KEY (`kode_paket`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -65,6 +132,12 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `guide`
+--
+ALTER TABLE `guide`
+  MODIFY `id_guide` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
