9bc11df8f906fa7796e655cd4351abedd93895a9
nabellapermata <nabellapermatas.if@gmail.com>
2018-12-16 12:02:25 +0700

    1634010048
    termcondition & customorder (npm 1634010050)

--- galeri-user.php
+++ galeri-user.php
@@ -8,6 +8,7 @@ if(!isset($_SESSION['user_id']) ){
 ?>
 <!DOCTYPE html>
 <html>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="assets/css/galeri5.css" rel="stylesheet">
 <link href="assets/css/bootstrap.min.css" rel="stylesheet">
